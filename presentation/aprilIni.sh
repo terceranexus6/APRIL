ec5d01258fa08ad0b8774d8f41736829aacee99f
Paula <terceranexus6@users.noreply.github.com>
2017-03-10 06:24:25 -0500

    Create aprilIni.sh

---
+++ presentation/aprilIni.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "           _____  _____  _____ _      "
+echo "     /\   |  __ \|  __ \|_   _| |     "
+echo "    /  \  | |__) | |__) | | | | |     "
+echo "   / /\ \ |  ___/|  _  /  | | | |     "
+echo "  / ____ \| |    | | \ \ _| |_| |____ "
+echo " /_/    \_\_|    |_|  \_\_____|______|"
+echo ""
+echo ""
+echo "CERTAMEN DE PROYECTOS DE SOFTWARE LIBRE"
+echo ""
+echo "**************************************"
+echo ""
+echo "Paula de la Hoz Garrido"
+echo "--------------------------"
+echo "Twitter, Github, Telegram:"
+echo "@Terceranexus6"
+echo "--------------------------"
+read -p"diap: " NUM
+
+./$NUM.sh
