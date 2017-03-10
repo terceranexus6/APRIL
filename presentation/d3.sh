adb91cd0e55334f96f397c1f64d667848b44803c
Paula <terceranexus6@users.noreply.github.com>
2017-03-10 06:25:56 -0500

    Create d3.sh

---
+++ presentation/d3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+
+echo "   _  _____                           __   ___  "
+echo "  (_)|  __ \                         /_/  |__ \ "
+echo "  | || |__) |__  _ __    __ _ _   _  ___     ) |"
+echo " / / |  ___/ _ \| |__|  / _  | | | |/ _ \   / / "
+echo "| (__| |  | (_) | |    | (_| | |_| |  __/  |_|  "
+echo " \___|_|   \___/|_|     \__, |\__,_|\___|  (_)  "
+echo "                           | |                  "
+echo "                           |_|                      "
+echo ""
+echo ""
+echo "--------------------------------------------------"
+echo ">>Recuperar el origen de los videojuegos con ayuda "
+echo "  de la comunidad."
+echo "--------------------------------------------------"
+echo ">>Funciona en Windows, Linux, Mac y con esfuerzo en"
+echo "  la DS y la PSP..."
+echo "  :P"
+echo "--------------------------"
+read -p"diap: " NUM
+
+./$NUM.sh
