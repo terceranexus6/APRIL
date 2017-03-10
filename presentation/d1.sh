2152b73bbc574f92b9ed1b0923bd1f0b31b8c2cd
Paula <terceranexus6@users.noreply.github.com>
2017-03-10 06:25:09 -0500

    Create d1.sh

---
+++ presentation/d1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo ">>INDEX"
+echo ""
+echo ""
+echo ">>1- Proyecto"
+echo ">>2- ¿Por qué una aventura de texto?"
+echo ">>3- ¿Cómo colaborar?"
+echo ""
+echo "------------------------------------"
+read -p"diap: " NUM
+
+./$NUM.sh
