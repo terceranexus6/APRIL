2f767bb49afb9aaeb69ae5088d61004b8f86b40c
Paula <terceranexus6@users.noreply.github.com>
2017-03-10 06:23:31 -0500

    Create trigger.sh

---
+++ presentation/trigger.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+clear
+echo ""
+sleep 2
+echo ">>Diario de investigación: 09/13/1983"
+echo "*************************************"
+sleep 2
+echo ">>La memoria... es algo fascinante."
+sleep 2
+echo ">>¿Qué somos si no un cúmulo de recuerdos?"
+sleep 2
+echo ">>Entendemos la realidad por lo que conocemos... ¿Cómo la entenderíamos sin nuestros recuerdos?"
+sleep 2
+echo ">>Creo que este experimento será decisivo para entenderlo"
+sleep 4
+clear
+echo "."
+sleep 1
+clear
+echo ".."
+sleep 1
+clear
+echo "..."
+sleep 1
+clear
+echo "."
+sleep 1
+clear
+echo ".."
+sleep 1
+clear
+echo "..."
+sleep 1
+clear
+sleep 2
+clear
+./aprilIni.sh
