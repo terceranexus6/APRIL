9d2a941103c56426647f4414dacbb07d84d97d49
Paula <terceranexus6@users.noreply.github.com>
2017-03-10 06:26:22 -0500

    Create d4.sh

---
+++ presentation/d4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+
+echo "  _____      _       _                          "
+echo " / ____|    | |     | |                         "
+echo "| |     ___ | | __ _| |__   ___  _ __ __ _ _ __ "
+echo "| |    / _ \| |/ _  v  _  v _  \|  __  _  |  __|"
+echo "| |___| (_) | | (_| | |_) | (_) | | | (_| | |   "
+echo " \_____\___/|_|\__,_|_.__/ \___/|_|  \__,_|_|   "
+echo ""
+echo "------------------------------------------------"
+echo ">>Mejorando el código"
+echo ">>Mejorando la web"
+echo ">>Aportando fanart"
+echo ">>Aportando texto, historias, memorias..."
+echo "--------------------------"
+read -p"diap: " NUM
+
+./$NUM.sh
